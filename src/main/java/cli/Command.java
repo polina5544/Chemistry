f60cf201d46bc6b1448cefade732241bda5d62b9
42191 <kolgina.07@gmail.com>
2026-03-28 13:53:06 +0300

    :3

---
+++ src/main/java/cli/Command.java
@@ -0,0 +1,12 @@
+package cli;
+
+import java.util.Scanner;
+
+public interface Command {
+    void execute(String[]args);
+    void validateArgs(String[]args);
+    String getHelp();
+    boolean equals(Object o);
+    int hashCode();
+    void startAdditionalInput(Scanner scanner);
+}
